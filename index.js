d66a19f20644b5f5fe1f5b1f0cf9ef246bd95384
Harrison Quamily <quamilyh@gmail.com>
2023-03-21 19:07:12 -0400

    function

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+const array1 = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (array1) {
+    return array1.slice(0,2)
+}
+const returnLastTwoDrivers = function (array1) {
+    return array1.slice(2,4)
+}    
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiplyValue) {
+    return function (value) {
+        return multiplyValue * value;
+      };
+    };
+
+    const fareDoubler = createFareMultiplier(2);
+    const fareTripler = createFareMultiplier(3);
+    const selectDifferentDrivers = function (drivers, driversToReturn) {
+        return driversToReturn(drivers);
+      };
+      
